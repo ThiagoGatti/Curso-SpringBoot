96584c9cc37ad0d65ee3e1e8ae7a15e0fed61cbb
Thiago <thiago.bafi@outlook.com>
2024-08-05 17:24:51 -0300

    Update UserResource.java

--- Curso/curso/src/main/java/com/estudo/curso/resources/UserResource.java
+++ Curso/curso/src/main/java/com/estudo/curso/resources/UserResource.java
@@ -1,18 +1,23 @@
 package com.estudo.curso.resources;
 
 import com.estudo.curso.entity.User;
+import com.estudo.curso.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "11-92939003", "123malu");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
