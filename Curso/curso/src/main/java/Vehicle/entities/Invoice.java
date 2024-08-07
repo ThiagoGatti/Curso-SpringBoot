5de7d2dd54cdf776949f9724d7fb7a28847cecea
Thiago <thiago.bafi@outlook.com>
2024-08-07 16:34:26 -0300

    interface add

--- Curso/curso/src/main/java/Vehicle/entities/Invoice.java
+++ Curso/curso/src/main/java/Vehicle/entities/Invoice.java
@@ -33,5 +33,5 @@ public class Invoice {
     public double getTotalPayment() {
         return getBasicPayment() + getTax();
     }
-    
+
 }
---
+++ Curso/curso/src/main/java/Vehicle/services/TaxService.java
@@ -0,0 +1,7 @@
+package Vehicle.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+
+}
