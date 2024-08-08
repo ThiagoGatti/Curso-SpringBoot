c877f2c772c854eb8af37ff75eaf708cbf30f81f
Thiago <thiago.bafi@outlook.com>
2024-08-08 10:19:31 -0300

    JsonIgnore para juntar dados

--- Curso/curso/src/main/java/com/estudo/curso/entity/Order.java
+++ Curso/curso/src/main/java/com/estudo/curso/entity/Order.java
@@ -1,4 +1,5 @@
 package com.estudo.curso.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -14,6 +15,7 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
