209ca401610633790a7802818d4eab4f4afaadde
Thiago <thiago.bafi@outlook.com>
2024-08-08 10:45:41 -0300

    Add Category Entity

--- Curso/curso/src/main/java/com/estudo/curso/config/TestConfig.java
+++ Curso/curso/src/main/java/com/estudo/curso/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.estudo.curso.config;
 
+import com.estudo.curso.entity.Category;
 import com.estudo.curso.entity.Order;
 import com.estudo.curso.entity.User;
 import com.estudo.curso.enums.OrderStatus;
+import com.estudo.curso.repositories.CategoryRepository;
 import com.estudo.curso.repositories.OrderRepository;
 import com.estudo.curso.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
@@ -30,6 +35,10 @@ public class TestConfig implements CommandLineRunner {
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.CANCELED, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u1);
+
+        Category cat1 = new Category(1, "Electronics");
+        Category cat2 = new Category(2, "Books");
+
 
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
