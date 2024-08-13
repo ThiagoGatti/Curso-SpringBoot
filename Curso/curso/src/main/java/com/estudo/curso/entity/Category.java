2a86ba9a0f3a6e5ab0158f7bbe9cdaa1c78224e2
Thiago <thiago.bafi@outlook.com>
2024-08-13 15:18:38 -0300

    ManyToMany @

--- Curso/curso/src/main/java/com/estudo/curso/entity/Category.java
+++ Curso/curso/src/main/java/com/estudo/curso/entity/Category.java
@@ -16,6 +16,7 @@ public class Category implements Serializable {
     private long id;
     private String name;
 
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Set<Product> getProducts() {
--- Curso/curso/src/main/java/com/estudo/curso/entity/Product.java
+++ Curso/curso/src/main/java/com/estudo/curso/entity/Product.java
@@ -80,7 +80,12 @@ public class Product implements Serializable {
     }
 
     @ManyToMany
-    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"))
+    @JoinTable
+            (
+            name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+            )
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
